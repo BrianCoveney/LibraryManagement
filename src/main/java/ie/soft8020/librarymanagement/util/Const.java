d4388a8b35958d6358b13e76e66ea79f42af803d
BrianCoveney <coveneybrian@gmail.com>
2018-07-13 17:19:25 +0100

    Work on abstract class and subclasses

--- src/main/java/ie/soft8020/librarymanagement/util/Const.java
+++ src/main/java/ie/soft8020/librarymanagement/util/Const.java
@@ -1,14 +1,14 @@
 package ie.soft8020.librarymanagement.util;
 
 public final class Const {
-	
+
 	public static class LoanLimit {
 		public static final int MAX_NUMBER_OF_BOOKS_FOR_ADULT = 4;
 		public static final int MAX_NUMBER_OF_BOOKS_FOR_CHILD = 2;
 	}
-	
+
 	public static class LoanLength {
-		public static final int MAX_NUMBER_OF_DAYS_ADULT_CAN_BORROW = 14;
-		public static final int MAX_NUMBER_OF_DAYS_CHILD_CAN_BORROW = 7;
+		public static final int MAX_LENGTH_OF_DAYS_ADULT_CAN_BORROW = 14;
+		public static final int MAX_LENGTH_OF_DAYS_CHILD_CAN_BORROW = 7;
 	}
 }
