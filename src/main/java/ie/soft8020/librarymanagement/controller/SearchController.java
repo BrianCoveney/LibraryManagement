412ac976590e7024f2a82c1ace50ce30e5b8173f
BrianCoveney <coveneybrian@gmail.com>
2018-07-26 14:38:07 +0100

    Add search all to controller

--- src/main/java/ie/soft8020/librarymanagement/controller/SearchController.java
+++ src/main/java/ie/soft8020/librarymanagement/controller/SearchController.java
@@ -1,13 +1,11 @@
 package ie.soft8020.librarymanagement.controller;
 
 import ie.soft8020.librarymanagement.domain.Book;
-import ie.soft8020.librarymanagement.domain.Member;
 import ie.soft8020.librarymanagement.repository.IBookRepository;
 import ie.soft8020.librarymanagement.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,17 +15,24 @@ import java.util.List;
 public class SearchController {
 
     @Autowired
-    IBookRepository bookService;
+    IBookService bookService;;
 
-    private List<Book> books;
+    private List<Book> bookListByAuthor;
+//    private List<Book> bookList;
 
     @GetMapping(value = "/search")
-    public String search(@RequestParam(value = "book", required = false) String author, Model model) {
+    public String searchBooksByAuthor(@RequestParam(value = "book", required = false) String author, Model model) {
         if (author != null) {
-            books = bookService.getBooksByAuthor(author);
+            bookListByAuthor = bookService.getBooksByAuthor(author);
         }
-        model.addAttribute("bookKey", books);
+
+        model.addAttribute("bookAuthor", bookListByAuthor);
+
+        model.addAttribute("booksAll", bookService.findAll());
+        
+
         return "search";
     }
+
 
 }
